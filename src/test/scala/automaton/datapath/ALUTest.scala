55abf6b6541adc68af9383eb8844b5d964532294
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-10-16 18:03:15 +0000

    ALU Test module

---
+++ src/test/scala/automaton/datapath/ALUTest.scala
@@ -0,0 +1,21 @@
+package automaton.datapath
+
+import org.scalatest._
+import chiseltest._
+import chisel3._
+
+class ALUTest extends FlatSpec with ChiselScalatestTester with Matchers {
+  behavior of "ALU"
+  it should "be able to add two register numbers" in {
+    test(new ALU) { c =>
+      {
+        c.io.a.poke(12.U)
+        c.io.b.poke(30.U)
+        c.io.aluCtl.poke(2.U)
+
+        c.io.result.expect(42.U)
+        c.io.zero.expect(false.B)
+      }
+    }
+  }
+}
