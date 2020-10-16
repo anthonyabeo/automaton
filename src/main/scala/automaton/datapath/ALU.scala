455c104064132e6b2c9f30dcce5a1c9b14d720de
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-10-16 18:46:37 +0000

    ALU to operate on 64-bit values

--- src/main/scala/automaton/datapath/ALU.scala
+++ src/main/scala/automaton/datapath/ALU.scala
@@ -5,13 +5,13 @@ import chisel3.util._
 
 class ALU extends Module {
   val io = IO(new Bundle {
-    val a = Input(UInt(32.W))
-    val b = Input(UInt(32.W))
+    val a = Input(UInt(64.W))
+    val b = Input(UInt(64.W))
     val aluCtl = Input(UInt(4.W))
 
     val zero = Output(Bool())
     val overflow = Output(Bool())
-    val result = Output(UInt(32.W))
+    val result = Output(UInt(64.W))
   })
 
   io.result := io.a + io.b
