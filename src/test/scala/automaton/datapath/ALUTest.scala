0d588c152c42f5ece4d7a925e63ae711290a2aef
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-10-19 15:42:35 +0000

    update simple add test

--- src/test/scala/automaton/datapath/ALUTest.scala
+++ src/test/scala/automaton/datapath/ALUTest.scala
@@ -11,11 +11,12 @@ class ALUTest extends FlatSpec with ChiselScalatestTester with Matchers {
       {
         c.io.a.poke(12.U)
         c.io.b.poke(30.U)
-        c.io.aluCtl.poke(2.U)
+        c.io.aluCtl.poke(0.U)
 
         c.io.result.expect(42.U)
         c.io.zero.expect(false.B)
         c.io.overflow.expect(false.B)
+        c.io.negative.expect(false.B)
       }
     }
   }
