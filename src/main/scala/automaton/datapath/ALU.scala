2b01d879fbda6ea9037592785c2832625112a85b
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-10-19 15:42:02 +0000

    basic ALU operations

--- src/main/scala/automaton/datapath/ALU.scala
+++ src/main/scala/automaton/datapath/ALU.scala
@@ -3,6 +3,8 @@ package automaton.datapath
 import chisel3._
 import chisel3.util._
 
+import datapath._
+
 class ALU extends Module {
   val io = IO(new Bundle {
     val a = Input(UInt(64.W))
@@ -11,10 +13,32 @@ class ALU extends Module {
 
     val zero = Output(Bool())
     val overflow = Output(Bool())
+    val negative = Output(Bool())
     val result = Output(UInt(64.W))
   })
 
-  io.result := io.a + io.b
-  io.overflow := false.B
+  val res = WireDefault(0.U(64.W))
+
+  switch(io.aluCtl) {
+    is(add) {
+      res := io.a + io.b
+    }
+    is(sub) {
+      res := io.a - io.b
+    }
+    is(and) {
+      res := io.a & io.b
+    }
+    is(or) {
+      res := io.a | io.b
+    }
+    is(xor) {
+      res := io.a ^ io.b
+    }
+  }
+
+  io.result := res
+  io.overflow := (io.a(0) === io.b(0)) && (io.result(0) != io.a(0))
+  io.negative := (io.result < 0.U)
   io.zero := (io.result === 0.U)
 }
