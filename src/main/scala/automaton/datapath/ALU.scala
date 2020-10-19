11fbd497e76fcbbe8a1e8d94062c11ded4219588
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-10-19 21:22:19 +0000

    make ALU generic operand widths

--- src/main/scala/automaton/datapath/ALU.scala
+++ src/main/scala/automaton/datapath/ALU.scala
@@ -5,40 +5,42 @@ import chisel3.util._
 
 import datapath._
 
-class ALU extends Module {
+class ALU(size: Int) extends Module {
   val io = IO(new Bundle {
-    val a = Input(UInt(64.W))
-    val b = Input(UInt(64.W))
+    val a = Input(Bits(size.W))
+    val b = Input(Bits(size.W))
     val aluCtl = Input(UInt(4.W))
 
     val zero = Output(Bool())
     val overflow = Output(Bool())
     val negative = Output(Bool())
-    val result = Output(UInt(64.W))
+    val result = Output(Bits(size.W))
   })
 
-  val res = WireDefault(0.U(64.W))
+  val a = io.a
+  val b = io.b
+  val res = WireDefault(0.U(size.W))
 
   switch(io.aluCtl) {
     is(add) {
-      res := io.a + io.b
+      res := a + b
     }
     is(sub) {
-      res := io.a - io.b
+      res := a - b
     }
     is(and) {
-      res := io.a & io.b
+      res := a & b
     }
     is(or) {
-      res := io.a | io.b
+      res := a | b
     }
     is(xor) {
-      res := io.a ^ io.b
+      res := a ^ b
     }
   }
 
   io.result := res
-  io.overflow := (io.a(0) === io.b(0)) && (io.result(0) != io.a(0))
-  io.negative := (io.result < 0.U)
-  io.zero := (io.result === 0.U)
+  io.overflow := false.B
+  io.negative := (res < 0.U)
+  io.zero := (res === 0.U)
 }
--- src/test/scala/automaton/datapath/ALUTest.scala
+++ src/test/scala/automaton/datapath/ALUTest.scala
@@ -7,7 +7,7 @@ import chisel3._
 class ALUTest extends FlatSpec with ChiselScalatestTester with Matchers {
   behavior of "ALU"
   it should "be able to add two register numbers" in {
-    test(new ALU) { c =>
+    test(new ALU(8)) { c =>
       {
         c.io.a.poke(12.U)
         c.io.b.poke(30.U)
@@ -20,4 +20,19 @@ class ALUTest extends FlatSpec with ChiselScalatestTester with Matchers {
       }
     }
   }
+
+  // it should "detect overflow after addition" in {
+  //   test(new ALU(5)) { c =>
+  //     {
+  //       c.io.a.poke("b10100".asUInt(5.W))
+  //       c.io.b.poke("b11010".asUInt(5.W))
+  //       c.io.aluCtl.poke(0.U)
+
+  //       c.io.result.expect("b01110".U)
+  //       c.io.zero.expect(false.B)
+  //       c.io.overflow.expect(true.B)
+  //       c.io.negative.expect(false.B)
+  //     }
+  //   }
+  // }
 }
