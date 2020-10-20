747ec565a87951e696bf7fd13956f0477cb51ae0
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-10-20 16:01:39 +0000

    create async dual-read port, sync single-write port register file

---
+++ src/main/scala/automaton/datapath/RegisterFile.scala
@@ -0,0 +1,28 @@
+package automaton.datapath
+
+import chisel3._
+
+class RegisterFile(size: Int) extends Module {
+  val io = IO(new Bundle {
+    val readReg1 = Input(UInt(5.W))
+    val readReg2 = Input(UInt(5.W))
+    val writeReg = Input(UInt(5.W))
+    val wrEna = Input(Bool())
+    val writeData = Input(SInt(size.W))
+
+    val readData1 = Output(SInt(size.W))
+    val readData2 = Output(SInt(size.W))
+  })
+
+  // Create Register File
+  val regFile = Mem(size, SInt(size.W))
+
+  // Asynchronous reads
+  io.readData1 := regFile.read(io.readReg1)
+  io.readData2 := regFile.read(io.readReg2)
+
+  // synchronous writes
+  when(io.wrEna) {
+    regFile.write(idx = io.writeReg, data = io.writeData)
+  }
+}
