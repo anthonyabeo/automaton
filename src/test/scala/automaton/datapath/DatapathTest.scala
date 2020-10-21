3f6ad78c6818daf3f68042c7924b3ec33915e9c8
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-10-21 17:14:44 +0000

    update datapath test to generate VCD outputs

--- src/test/scala/automaton/datapath/DatapathTest.scala
+++ src/test/scala/automaton/datapath/DatapathTest.scala
@@ -4,21 +4,24 @@ import org.scalatest._
 
 import chisel3._
 import chiseltest._
+import chiseltest.internal.WriteVcdAnnotation
+import chiseltest.experimental.TestOptionBuilder._
 
 class DatapathTest extends FlatSpec with ChiselScalatestTester with Matchers {
   behavior.of("Datapath")
 
-  "Datapath" should "perform register add instruction" in {
-    test(new Datapath(size = 32, regWidth = 5)) { dp =>
-      dp.io.regWrite.poke(true.B)
-      dp.io.aluCtl.poke(0.U)
-      dp.io.memWrite.poke(false.B)
+  "Datapath" should "perform register ADD instruction" in {
+    test(new Datapath(size = 32, regWidth = 5)).withAnnotations(Seq(WriteVcdAnnotation)) { dp =>
+      {
+        dp.io.regWrite.poke(true.B)
+        dp.io.aluCtl.poke(0.U)
+        dp.io.memWrite.poke(false.B)
 
-      dp.clock.step(1)
+        dp.clock.step(1)
 
-      dp.io.reg.expect(11.S)
-      dp.io.pc.expect(4.U)
-
+        dp.io.reg.expect(11.S)
+        dp.io.pc.expect(4.U)
+      }
     }
   }
 }
