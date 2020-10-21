9b8759dea0f3a51705c98e59669fac6164ab4808
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-10-21 17:13:21 +0000

    rename Mem to avoid confusion with Chisel eponymous built-in type

--- src/main/scala/automaton/datapath/Memory.scala
+++ src/main/scala/automaton/datapath/Memory.scala
@@ -4,6 +4,8 @@ import chisel3._
 import chisel3.util.experimental.loadMemoryFromFile
 
 class DataCache(size: Int, addrWidth: Int) extends Module {
+  val NUM_REGISTERS = 32
+
   val io = IO(new Bundle {
     val addr = Input(UInt(addrWidth.W))
     val wrEna = Input(Bool())
@@ -12,24 +14,26 @@ class DataCache(size: Int, addrWidth: Int) extends Module {
     val dataOUT = Output(UInt(size.W))
   })
 
-  val Mem = SyncReadMem(size, UInt(size.W))
+  val mem = SyncReadMem(NUM_REGISTERS, UInt(size.W))
 
   when(io.wrEna) {
-    Mem.write(io.addr, io.dataIN)
+    mem.write(io.addr, io.dataIN)
   }
 
-  io.dataOUT := Mem.read(io.addr)
+  io.dataOUT := mem.read(io.addr)
 }
 
 class InstrCache(size: Int, addrWidth: Int) extends Module {
+  val NUM_REGISTERS = 32
+
   val io = IO(new Bundle {
     val addr = Input(UInt(addrWidth.W))
     val dataOUT = Output(UInt(size.W))
   })
 
-  val Mem = SyncReadMem(size, UInt(size.W))
+  val mem = SyncReadMem(NUM_REGISTERS, UInt(size.W))
 
-  loadMemoryFromFile(Mem, "src/main/resources/data/instr.txt")
+  loadMemoryFromFile(mem, "src/main/resources/data/instr.txt")
 
-  io.dataOUT := Mem.read(io.addr)
+  io.dataOUT := mem.read(io.addr)
 }
