67e00fa268646bc9f4a3fffd8802f4fa059e5e74
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-10-22 15:12:26 +0000

    remove regWidth parameter from constructors

--- src/test/scala/automaton/datapath/DatapathTest.scala
+++ src/test/scala/automaton/datapath/DatapathTest.scala
@@ -11,11 +11,12 @@ class DatapathTest extends FlatSpec with ChiselScalatestTester with Matchers {
   behavior.of("Datapath")
 
   "Datapath" should "perform register ADD instruction" in {
-    test(new Datapath(size = 32, regWidth = 5)).withAnnotations(Seq(WriteVcdAnnotation)) { dp =>
+    test(new Datapath(size = 32)).withAnnotations(Seq(WriteVcdAnnotation)) { dp =>
       {
         dp.io.regWrite.poke(true.B)
         dp.io.aluCtl.poke(0.U)
         dp.io.memWrite.poke(false.B)
+        dp.io.aluSrcB.poke(0.U)
 
         dp.clock.step(1)
 
@@ -24,4 +25,22 @@ class DatapathTest extends FlatSpec with ChiselScalatestTester with Matchers {
       }
     }
   }
+
+  // it should "perform register ADDI operation" in {
+  //   test(new Datapath(size = 32, regWidth = 5)).withAnnotations(Seq(WriteVcdAnnotation)) { dp =>
+  //     {
+  //       dp.clock.step(1)
+
+  //       dp.io.regWrite.poke(true.B)
+  //       dp.io.aluCtl.poke(0.U)
+  //       dp.io.memWrite.poke(false.B)
+  //       dp.io.aluSrcB.poke(1.U)
+
+  //       dp.clock.step(1)
+
+  //       dp.io.reg.expect(17.S)
+  //       dp.io.pc.expect(8.U)
+  //     }
+  //   }
+  // }
 }
