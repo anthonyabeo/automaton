6625d1de3cfac1872f96883482de032b08e30218
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-10-22 15:14:41 +0000

    define sign extension method

--- src/main/scala/automaton/datapath/package.scala
+++ src/main/scala/automaton/datapath/package.scala
@@ -1,5 +1,19 @@
 package object datapath {
-  import chisel3.util.Enum
+  import chisel3._
+  import chisel3.util._
 
   val add :: sub :: and :: or :: xor :: Nil = Enum(5)
+
+  def signExt(n: UInt): SInt = {
+    val bitString = "b0".U
+    when(isPos(n)) {
+      val bitString = "b0".U
+    }.otherwise {
+      val bitString = "b1".U
+    }
+
+    Cat(Fill(20, bitString), n).asSInt
+  }
+
+  def isPos(n: UInt): Bool = ((1.U << 31) & n) === 0.U
 }
