db30027d3bac0489bc215ffde9f646785bba6d65
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-10-22 16:57:02 +0000

    test AND and OR instructions

--- src/test/scala/automaton/datapath/DatapathTest.scala
+++ src/test/scala/automaton/datapath/DatapathTest.scala
@@ -44,4 +44,40 @@ class DatapathTest extends FlatSpec with ChiselScalatestTester with Matchers {
       }
     }
   }
+
+  it should "perform AND operation" in {
+    test(new Datapath(XLEN = 32)).withAnnotations(Seq(WriteVcdAnnotation)) { dp =>
+      // Delay by 2 clock cycle to execute first 2 instruction
+      dp.clock.step(1)
+      dp.clock.step(1)
+
+      dp.io.regWrite.poke(true.B)
+      dp.io.aluCtl.poke(2.U)
+      dp.io.memWrite.poke(false.B)
+      dp.io.aluSrcB.poke(0.U)
+
+      dp.clock.step(1)
+
+      dp.io.reg.expect(4.S)
+      dp.io.pc.expect(3.U)
+    }
+  }
+
+  it should "perform OR operation" in {
+    test(new Datapath(XLEN = 32)).withAnnotations(Seq(WriteVcdAnnotation)) { dp =>
+      // Delay by 3 clock cycle to execute first 3 instruction
+      dp.clock.step(1)
+      dp.clock.step(1)
+      dp.clock.step(1)
+
+      dp.io.regWrite.poke(true.B)
+      dp.io.aluCtl.poke(3.U)
+      dp.io.aluSrcB.poke(0.U)
+
+      dp.clock.step(1)
+
+      dp.io.reg.expect(7.S)
+      dp.io.pc.expect(4.U)
+    }
+  }
 }
