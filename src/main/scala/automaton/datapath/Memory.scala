eeab4ea089d56e805c5ceb4ce59e2f946bf5d350
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-10-22 15:14:04 +0000

    change memory size to 1024

--- src/main/scala/automaton/datapath/Memory.scala
+++ src/main/scala/automaton/datapath/Memory.scala
@@ -1,20 +1,21 @@
 package automaton.datapath
 
 import chisel3._
+import chisel3.util.log2Ceil
 import chisel3.util.experimental.loadMemoryFromFile
 
-class DataCache(size: Int, addrWidth: Int) extends Module {
-  val NUM_REGISTERS = 32
+class DataCache(size: Int) extends Module {
+  val CAPACITY = 1024
 
   val io = IO(new Bundle {
-    val addr = Input(UInt(addrWidth.W))
+    val addr = Input(UInt(log2Ceil(CAPACITY).W))
     val wrEna = Input(Bool())
     val dataIN = Input(UInt(size.W))
 
     val dataOUT = Output(UInt(size.W))
   })
 
-  val mem = SyncReadMem(NUM_REGISTERS, UInt(size.W))
+  val mem = SyncReadMem(CAPACITY, UInt(size.W))
 
   when(io.wrEna) {
     mem.write(io.addr, io.dataIN)
@@ -23,15 +24,15 @@ class DataCache(size: Int, addrWidth: Int) extends Module {
   io.dataOUT := mem.read(io.addr)
 }
 
-class InstrCache(size: Int, addrWidth: Int) extends Module {
-  val NUM_REGISTERS = 32
+class InstrCache(size: Int) extends Module {
+  val CAPACITY = 1024
 
   val io = IO(new Bundle {
-    val addr = Input(UInt(addrWidth.W))
+    val addr = Input(UInt(log2Ceil(CAPACITY).W))
     val dataOUT = Output(UInt(size.W))
   })
 
-  val mem = SyncReadMem(NUM_REGISTERS, UInt(size.W))
+  val mem = SyncReadMem(CAPACITY, UInt(size.W))
 
   loadMemoryFromFile(mem, "src/main/resources/data/instr.txt")
 
