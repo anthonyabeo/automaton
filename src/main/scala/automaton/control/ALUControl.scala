dc1ac2097b029ac0f66eaaf6d4fc6d258c671630
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-11-09 09:39:11 +0000

    partition ALU ctl signal generation by instruction type

--- src/main/scala/automaton/control/ALUControl.scala
+++ src/main/scala/automaton/control/ALUControl.scala
@@ -13,46 +13,52 @@ class ALUControl extends Module {
   })
 
   val out = WireDefault(0.U(3.W))
-  switch(Cat(io.aluOp, io.funct7, io.funct3)) {
-    is("b0000000000000".U) {
-      out := 0.U
+  switch(io.aluOp) {
+    is("b000".U) {
+      switch(Cat(io.funct7, io.funct3)) {
+        is("b0000000000".U) {
+          out := 0.U
+        }
+
+        is("b0100000000".U) {
+          out := 1.U
+        }
+
+        is("b0000000001".U) {
+          out := 5.U
+        }
+
+        is("b0000000010".U) {
+          out := 1.U
+        }
+
+        is("b0000000011".U) {
+          out := 1.U
+        }
+
+        is("b0000000100".U) {
+          out := 4.U
+        }
+
+        is("b0000000101".U) {
+          out := 6.U
+        }
+
+        is("b0100000101".U) {
+          out := 7.U
+        }
+
+        is("b0000000110".U) {
+          out := 3.U
+        }
+
+        is("b0000000111".U) {
+          out := 2.U
+        }
+      }
     }
 
-    is("b0000100000000".U) {
-      out := 1.U
-    }
-
-    is("b0000000000001".U) {
-      out := 5.U
-    }
-
-    is("b0000000000010".U) {
-      out := 1.U
-    }
-
-    is("b0000000000011".U) {
-      out := 1.U
-    }
-
-    is("b0000000000100".U) {
-      out := 4.U
-    }
-
-    is("b0000000000101".U) {
-      out := 6.U
-    }
-
-    is("b0000100000101".U) {
-      out := 7.U
-    }
-
-    is("b0000000000110".U) {
-      out := 3.U
-    }
-
-    is("b0000000000111".U) {
-      out := 2.U
-    }
+    is("b001".U) {}
   }
 
   io.aluCtl := out
