22ae6674553bf84f794e45efcda553a8b23901f8
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-11-09 10:57:13 +0000

    main controller generate ALU control signals

--- src/main/scala/automaton/control/MainControl.scala
+++ src/main/scala/automaton/control/MainControl.scala
@@ -10,11 +10,31 @@ class MainControl extends Module {
     val aluOp = Output(UInt(3.W))
   })
 
+  val out = WireDefault(0.U(3.W))
+
   switch(io.opcode) {
     is("b0110011".U) {
-      io.aluOp := 0.U
+      out := 0.U
     }
-
-    // is("b0010011".U) {}
+    is("b0010011".U) {
+      out := 1.U
+    }
+    is("b1100011".U) {
+      out := 2.U
+    }
+    is("b0000011".U) {
+      out := 3.U
+    }
+    is("b0100011".U) {
+      out := 4.U
+    }
+    is("b1101111".U) {
+      out := 5.U
+    }
+    is("b1100111".U) {
+      out := 6.U
+    }
   }
+
+  io.aluOp := out
 }
--- src/test/scala/automaton/control/MainControlTest.scala
+++ src/test/scala/automaton/control/MainControlTest.scala
@@ -1,1 +1,44 @@
 package automaton.control
+
+import org.scalatest._
+
+import chisel3._
+import chiseltest._
+import chiseltest.internal.WriteVcdAnnotation
+import chiseltest.experimental.TestOptionBuilder._
+
+class MainControlTest extends FlatSpec with ChiselScalatestTester {
+  behavior.of("Main Controller")
+
+  it should "generate correct signals based on opcode" in {
+    test(new MainControl).withAnnotations(Seq(WriteVcdAnnotation)) { ctl =>
+      // Reguster-Register
+      ctl.io.opcode.poke("b0110011".U)
+      ctl.io.aluOp.expect(0.U)
+
+      // Register-Immediate
+      ctl.io.opcode.poke("b0010011".U)
+      ctl.io.aluOp.expect(1.U)
+
+      // Branch
+      ctl.io.opcode.poke("b1100011".U)
+      ctl.io.aluOp.expect(2.U)
+
+      // LOAD
+      ctl.io.opcode.poke("b0000011".U)
+      ctl.io.aluOp.expect(3.U)
+
+      // STORE
+      ctl.io.opcode.poke("b0100011".U)
+      ctl.io.aluOp.expect(4.U)
+
+      // JAL
+      ctl.io.opcode.poke("b1101111".U)
+      ctl.io.aluOp.expect(5.U)
+
+      // JAL
+      ctl.io.opcode.poke("b1100111".U)
+      ctl.io.aluOp.expect(6.U)
+    }
+  }
+}
