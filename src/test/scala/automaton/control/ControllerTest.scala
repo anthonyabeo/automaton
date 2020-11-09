2463cedf028b71f0e2ef3760ccbdfb65c74f8a03
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-11-09 11:53:52 +0000

    test controller signal generation for Register-Immediate operations

--- src/test/scala/automaton/control/ControllerTest.scala
+++ src/test/scala/automaton/control/ControllerTest.scala
@@ -80,73 +80,61 @@ class ControllerTest extends FlatSpec with ChiselScalatestTester {
     }
   }
 
-//   it should "generate control signals for Register-Intermediate" in {
-//     test(new Controller).withAnnotations(Seq(WriteVcdAnnotation)) { ctl =>
-//       ctl.io.opcode.poke("b0110011".U)
-//       //////////
-//       // ADD
-//       //////////
-//       ctl.io.funct3.poke("b000".U)
-//       ctl.io.funct7.poke("b0000000".U)
+  it should "generate control signals for Register-Intermediate" in {
+    test(new Controller).withAnnotations(Seq(WriteVcdAnnotation)) { ctl =>
+      ctl.io.opcode.poke("b0010011".U)
+      //////////
+      // ADDI
+      //////////
+      ctl.io.funct3.poke("b000".U)
 
-//       ctl.io.aluCtl.expect(0.U)
+      ctl.io.aluCtl.expect(0.U)
 
-//       //////////
-//       // SUB
-//       //////////
-//       ctl.io.funct3.poke("b000".U)
-//       ctl.io.funct7.poke("b0100000".U)
+      //////////
+      // SLLI
+      //////////
+      ctl.io.funct3.poke("b001".U)
+      ctl.io.funct7.poke("b0000000".U)
 
-//       ctl.io.aluCtl.expect(1.U)
+      ctl.io.aluCtl.expect(5.U)
 
-//       //////////
-//       // SLL
-//       //////////
-//       ctl.io.funct3.poke("b001".U)
-//       ctl.io.funct7.poke("b0000000".U)
+      //////////
+      // SLTI[U]
+      //////////
+      ctl.io.funct3.poke("b010".U)
+      ctl.io.aluCtl.expect(1.U)
 
-//       ctl.io.aluCtl.expect(5.U)
+      ctl.io.funct3.poke("b011".U)
+      ctl.io.funct7.poke("b0000000".U)
+      ctl.io.aluCtl.expect(1.U)
 
-//       //////////
-//       // SLT[U]
-//       //////////
-//       ctl.io.funct3.poke("b010".U)
-//       ctl.io.funct7.poke("b0000000".U)
+      //////////
+      // XOR
+      //////////
+      ctl.io.funct3.poke("b100".U)
 
-//       ctl.io.aluCtl.expect(1.U)
+      ctl.io.aluCtl.expect(4.U)
 
-//       ctl.io.funct3.poke("b011".U)
-//       ctl.io.aluCtl.expect(1.U)
+      /////////////
+      // SR[L|A]I //
+      /////////////
+      ctl.io.funct3.poke("b101".U)
+      ctl.io.funct7.poke("b0000000".U)
 
-//       //////////
-//       // XOR
-//       //////////
-//       ctl.io.funct3.poke("b100".U)
-//       ctl.io.funct7.poke("b0000000".U)
+      ctl.io.aluCtl.expect(6.U)
 
-//       ctl.io.aluCtl.expect(4.U)
+      ctl.io.funct7.poke("b0100000".U)
+      ctl.io.aluCtl.expect(7.U)
 
-//       /////////////
-//       // SR[L|A] //
-//       /////////////
-//       ctl.io.funct3.poke("b101".U)
-//       ctl.io.funct7.poke("b0000000".U)
+      //////////////
+      // ORI | ANDI
+      //////////////
+      ctl.io.funct3.poke("b110".U)
+      ctl.io.aluCtl.expect(3.U)
 
-//       ctl.io.aluCtl.expect(6.U)
+      ctl.io.funct3.poke("b111".U)
+      ctl.io.aluCtl.expect(2.U)
 
-//       ctl.io.funct7.poke("b0100000".U)
-//       ctl.io.aluCtl.expect(7.U)
-
-//       //////////////
-//       // OR | AND
-//       /////////////
-//       ctl.io.funct3.poke("b110".U)
-//       ctl.io.funct7.poke("b0000000".U)
-//       ctl.io.aluCtl.expect(3.U)
-
-//       ctl.io.funct3.poke("b111".U)
-//       ctl.io.aluCtl.expect(2.U)
-
-//     }
-//   }
+    }
+  }
 }
