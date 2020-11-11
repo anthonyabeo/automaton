fa84fff3570b213a11950eeed55782707e513dce
Abeo Anthony, A <anthonyabeo@gmail.com>
2020-11-11 11:56:49 +0000

    test automaton with fibonacci of 8

---
+++ src/test/scala/automaton/AutomatonTest.scala
@@ -0,0 +1,20 @@
+package automaton
+
+import org.scalatest._
+
+import firrtl.transforms.NoDCEAnnotation
+
+import chisel3._
+import chiseltest._
+import chiseltest.internal.WriteVcdAnnotation
+import chiseltest.experimental.TestOptionBuilder._
+
+class AutomatonTest extends FlatSpec with ChiselScalatestTester {
+  behavior.of("Automaton")
+
+  it should "compute fib of 8" in {
+    test(new Automaton(XLEN = 32)).withAnnotations(Seq(WriteVcdAnnotation, NoDCEAnnotation)) { auto =>
+      auto.clock.step(50)
+    }
+  }
+}
